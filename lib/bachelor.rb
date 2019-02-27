59ee6ca3b5f07eda5cf5cdd175a8850e6e1d54c8
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-27 03:16:22 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,18 +1,87 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+winner = ""
+
+  data.collect do |time, stats|
+# puts " I NEED THIS TO BE TRUE #{season === time}"
+    if season == time
+
+      stats.collect do |i|
+      i.collect do |a, b|
+
+      if b == "Winner"
+      winner = i.values[0].split(" ")
+      puts winner[0]
+      end
+      end
+      end
+    end
+  end
+return winner[0]
 end
 
-def get_contestant_name(data, occupation)
-  # code here
+
+
+  def get_contestant_name(data, job)
+lady = ""
+
+  data.collect do |time, stats|
+
+      stats.collect do |i|
+      i.collect do |a, b|
+      
+      if b == job
+      puts "#{b} BBBBB"
+      lady = i.values[0]
+  
+      # end
+      end
+      end
+     end
+  end
+lady
 end
 
-def count_contestants_by_hometown(data, hometown)
-  # code here
+
+def count_contestants_by_hometown(data, town)
+count = 0
+
+  data.collect do |time, stats|
+
+      stats.collect do |i|
+      i.collect do |a, b|
+      
+      if b == town
+      count += 1
+  
+      # end
+      end
+      end
+     end
+  end
+count
 end
 
-def get_occupation(data, hometown)
-  # code here
-end
+def get_occupation(data, town)
+  lady_names = []
+
+  data.collect do |time, stats|
+      stats.collect do |i|
+      i.collect do |a, b|
+      
+      if b == town
+      lady_names << i.values[3]
+      
+      end
+      end
+      end
+     end
+     lady_names[0]
+  end
+  
+
+
+
+
 
 def get_average_age_for_season(data, season)
   # code here
